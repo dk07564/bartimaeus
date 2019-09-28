0f0ad8a8d40c4981ba1454e7a62e13c54cda6391
dk07564 <52230701+dk07564@users.noreply.github.com>
2019-09-29 01:53:50 +0900

    바코드 인식을 위한 카메라 생성

--- app/src/main/java/com/bartimaeus/MainActivity.java
+++ app/src/main/java/com/bartimaeus/MainActivity.java
@@ -3,12 +3,21 @@ package com.bartimaeus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity {
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qrScan= new IntentIntegrator(this);
+
+        qrScan.initiateScan();
     }
+
 }
