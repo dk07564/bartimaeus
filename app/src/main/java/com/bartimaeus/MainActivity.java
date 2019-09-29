7d6458e698169d3817499c186d21b49067bdda65
dk07564 <52230701+dk07564@users.noreply.github.com>
2019-09-29 11:41:30 +0900

    바코드 인식

--- app/src/main/java/com/bartimaeus/MainActivity.java
+++ app/src/main/java/com/bartimaeus/MainActivity.java
@@ -2,22 +2,37 @@ package com.bartimaeus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private IntentIntegrator qrScan;
+    TextView scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        scan=findViewById(R.id.textView);
+
         qrScan= new IntentIntegrator(this);
 
         qrScan.initiateScan();
     }
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
 
-}
+               IntentResult result=IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+               scan.setText(result.getContents());
+       }
+    }
+
+
