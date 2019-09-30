df1e61badbe177045b867136a80f4baa069d6bfd
dk07564 <52230701+dk07564@users.noreply.github.com>
2019-09-30 18:23:57 +0900

    바코드 세로로 바꿈

---
+++ app/src/main/java/com/bartimaeus/AnyOrientationCaptureActivity.java
@@ -0,0 +1,6 @@
+package com.bartimaeus;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+}
--- app/src/main/java/com/bartimaeus/MainActivity.java
+++ app/src/main/java/com/bartimaeus/MainActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
     private IntentIntegrator qrScan;
     TextView scan;
+    String barCord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +24,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         scan=findViewById(R.id.textView);
-
         qrScan= new IntentIntegrator(this);
-
+        qrScan.setCaptureActivity(AnyOrientationCaptureActivity.class);
+        qrScan.setOrientationLocked(false);
         qrScan.initiateScan();
+
+
     }
+
     @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-               IntentResult result=IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
-               scan.setText(result.getContents());
-       }
+        barCord=(String.valueOf(result.getContents()));
+
+        scan.setText(barCord);
     }
+
+    }
+
 
 
