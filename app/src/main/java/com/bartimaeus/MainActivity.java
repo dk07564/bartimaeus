a99abec3517314e0f04b687bd7650e191b0fc3b2
dk07564 <52230701+dk07564@users.noreply.github.com>
2019-10-01 22:30:40 +0900

    바코드 인식 및 TTS 구현

--- app/src/main/java/com/bartimaeus/MainActivity.java
+++ app/src/main/java/com/bartimaeus/MainActivity.java
@@ -1,48 +1,64 @@
 package com.bartimaeus;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.view.View;
-import android.widget.Button;
+import android.speech.tts.TextToSpeech;
 import android.widget.TextView;
-
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-import com.journeyapps.barcodescanner.CaptureActivity;
+
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
     private IntentIntegrator qrScan;
+    private IntentIntegrator Scan;
+    private TextToSpeech tts;
     TextView scan;
+    String barCordNumber;
     String barCord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         scan=findViewById(R.id.textView);
-        qrScan= new IntentIntegrator(this);
-        qrScan.setCaptureActivity(AnyOrientationCaptureActivity.class);
-        qrScan.setOrientationLocked(false);
-        qrScan.initiateScan();
+
+        tts= new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                tts.setLanguage(Locale.KOREA);
+            }
+        });
 
 
     }
 
     @Override
     protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
-        barCord=(String.valueOf(result.getContents()));
+        barCordNumber=(String.valueOf(result.getContents()));
 
-        scan.setText(barCord);
+        barCord="맥심\n티오피\n스위트 아메리카노";
+        if(barCordNumber.equals("8801037057843")){
+            scan.setText(barCord);
+
+            tts.speak(barCord,TextToSpeech.QUEUE_FLUSH, null);
+        }else{
+            scan.setText(barCordNumber);
+        }
+
+        //scan.setText(barCordNumber);
+        qrScan= new IntentIntegrator(this);
+        qrScan.setCaptureActivity(AnyOrientationCaptureActivity.class);
+        qrScan.setOrientationLocked(false);
+        qrScan.initiateScan();
+
     }
 
-    }
+}
 
 
 
