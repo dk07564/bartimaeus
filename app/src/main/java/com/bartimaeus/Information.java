644911096568b9009d583699401c9e3ae0e24d17
dk07564 <52230701+dk07564@users.noreply.github.com>
2019-10-03 16:33:43 +0900

    주석처리

--- app/src/main/java/com/bartimaeus/Information.java
+++ app/src/main/java/com/bartimaeus/Information.java
@@ -1,13 +1,10 @@
 package com.bartimaeus;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.widget.TextView;
-
 import java.util.Locale;
 
 public class Information extends AppCompatActivity {
@@ -21,14 +18,14 @@ public class Information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
-
         scan=findViewById(R.id.textView);
 
+        //MainActivity에서 값을 전달 받음
         intent=getIntent();
         barCodeNumber=intent.getStringExtra("barCodeNumber");
-
         barCode="맥심\n티오피\n스위트 아메리카노";
 
+        //TTS기능 처리
         tts= new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int i) {
@@ -43,11 +40,6 @@ public class Information extends AppCompatActivity {
                 }
             }
         });
-
-
-
-
-
     }
 
     @Override
--- app/src/main/java/com/bartimaeus/MainActivity.java
+++ app/src/main/java/com/bartimaeus/MainActivity.java
@@ -3,18 +3,11 @@ package com.bartimaeus;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import java.util.Locale;
-
-
 public class MainActivity extends AppCompatActivity {
-    private IntentIntegrator barCodeScan;
+    private IntentIntegrator barCodeScan = new IntentIntegrator(this);
     private Intent intent;
 
     String barCodeNumber;
@@ -23,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setBarCodeScan();
     }
 
@@ -31,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
     }
 
+    //바코드가 스캔되었을 때 결과값을 넘겨줌
     @Override
     protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
@@ -42,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //바코드 스캔을 위한 함수
     public  void setBarCodeScan(){
-        barCodeScan= new IntentIntegrator(this);
+        //barCodeScan= new IntentIntegrator(this);
         barCodeScan.setCaptureActivity(AnyOrientationCaptureActivity.class);
         barCodeScan.setOrientationLocked(false);
         barCodeScan.initiateScan();
